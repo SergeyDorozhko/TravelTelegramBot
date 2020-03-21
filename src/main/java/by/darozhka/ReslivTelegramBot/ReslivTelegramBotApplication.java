c82699bc61f8093b7b1b789242408bd00161e7fe
SergeyDorozhko <sergdorozhko@gmail.com>
2020-03-21 23:23:23 +0300

    Create repository

---
+++ src/main/java/by/darozhka/ReslivTelegramBot/ReslivTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package by.darozhka.ReslivTelegramBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReslivTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReslivTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/darozhka/ReslivTelegramBot/ReslivTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package by.darozhka.ReslivTelegramBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReslivTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
